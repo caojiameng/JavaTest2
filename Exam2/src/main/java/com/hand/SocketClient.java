b6a972e1f7b2a887d53a3941c343250cf79fe812
caojiameng <jiameng.cao@hand-china.com>
2018-07-21 12:43:43 +0800

    第二题

--- Exam2/src/main/java/com/hand/SocketClient.java
+++ Exam2/src/main/java/com/hand/SocketClient.java
@@ -1,4 +1,34 @@
 package com.hand;
 
-public class SocketClient {
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class SocketClient extends Socket {
+
+
+    public static void main(String[] args) {
+        File dir = new File("Exam2/tmp");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        try {
+            Socket client = new Socket("127.0.0.1", 1024);
+            BufferedInputStream bis = new BufferedInputStream(client.getInputStream());
+            FileOutputStream fos =new FileOutputStream("Exam2/tmp/SampleChapter1.pdf");
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            byte[] input = new byte[1024];
+            int len;
+            while ((len = bis.read(input)) != -1) {
+                bos.write(input, 0, len);
+            }
+            bos.flush();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("文件读取完成，关闭连接");
+    }
 }
--- Exam2/src/main/java/com/hand/SocketServer.java
+++ Exam2/src/main/java/com/hand/SocketServer.java
@@ -1,4 +1,35 @@
 package com.hand;
 
-public class SocketServer {
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import java.io.*;
+import java.math.RoundingMode;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.DecimalFormat;
+
+public class SocketServer  {
+    public static void main(String[] args) {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(1024);
+            Socket socket = serverSocket.accept();
+            BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
+            FileInputStream fis= new FileInputStream("Exam1/tmp/SampleChapter1.pdf");
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            System.out.println("客户端连接建立");
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = bis.read(buf)) != -1) {
+                bos.write(buf, 0, len);
+            }
+            bos.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("已发送，关闭");
+    }
 }
